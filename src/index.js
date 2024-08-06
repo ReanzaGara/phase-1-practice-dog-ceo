7179f6ad259f5b69faaa04ed413167af1f0f43d9
+ <+>
2024-08-06 16:44:20 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(response => response.json())
+.then(data => {
+    data.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image.url;
+        document.body.appendChild(imgElement);
+    })
+})
